5e465581c903c4e9852456a9b2f71f4fd071ca44
nikh9123 <satyamnikhil1523@gmail.com>
2023-11-27 13:10:50 +0530

    typescript basics

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+  title :string = 'Appointment List'; // <-- Add this line
+  message :string= 'Hello ' ;
+  items : string[] = ['item1', 'item2', 'item3'];
+  log(){
+    console.log('hello');
+  }
+  name(text:string, name:string) : void{
+    var message = this.message + name ;
+    var message2 = 'Message' + text;
+    console.log(message);
+    console.log(message2);
+  }
 
+  
 }
